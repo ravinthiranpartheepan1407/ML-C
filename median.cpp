a64439a91a0204e9b40434378132d0a50be9c9d1
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-10-08 11:45:37 +0300

    median

---
+++ median.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+double median(vector<double> input){
+    double medianVal;
+    for(size_t elements = 0; elements < input.size(); elements++){
+        sort(input.begin(), input.end());
+        cout<<"The sorted values are: "<<input[elements]<<endl;
+        if(input.size() % 2 == 0){
+            double out = round((input.size() / 2));
+            cout<<"Prev Index: "<<input[out]<<endl;
+            double next = round((input.size() / 2) - 1); 
+            cout<<"Prev Index: "<<input[next]<<endl;
+            double medn = (input[out]+input[next])/2;
+            medianVal = medn;
+        }else{
+            double out = round((input.size() / 2));
+            double medn = (input[out]);
+            medianVal = medn;
+        }
+    }
+    cout<<"The median value is: ";
+    return medianVal;
+}
+
+int main(){
+    vector<double> input = {50,25,45,60,75,80,95,40};
+    // 25,40,45,50,60,75,80,95
+    cout<<median(input);
+}
