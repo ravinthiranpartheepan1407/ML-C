9ed5e1dee75f3253b82753697f93807289468d49
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-10-08 18:27:52 +0300

    Range

---
+++ range.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+int range(vector<double> x){
+    int maxValue = 0;
+    int minValue = x[0]; 
+    for(size_t elements = 1; elements < x.size(); elements++){
+        if(x[elements] > maxValue){
+            maxValue = x[elements];
+        }
+    }
+    for(size_t elements = 0; elements < x.size(); elements++){
+        if(x[elements] < minValue){
+            minValue = x[elements];
+        }
+    }
+    int rangeOut = maxValue - minValue;
+    cout<<"The max value is: "<<maxValue<<endl;
+    cout<<"The min value is: "<<minValue<<endl;
+    return rangeOut;
+}
+
+int main(){
+    vector<double> x = {40,10,20,30,60,40,50};
+    cout<<range(x);
+}
