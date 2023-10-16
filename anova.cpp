5a7d01d3d22ff71e6b9b7b73c1e7ddaf6303bbbb
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-10-16 19:30:37 +0300

    ANOVA

--- anova.cpp
+++ anova.cpp
@@ -4,33 +4,21 @@
 
 using namespace std;
 
-void anova(vector<double> x, vector<double> y, vector<double> z){
+double anova(vector<double> x, vector<double> y){
     // Each Groups: SSW
         // Sum
-        double sumX, sumY, sumZ, meanXVal, meanYVal, meanZVal, sampXMean, sampYMean, sampZMean, xSSWG, ySSWG, zSSWG, xMeanTot, yMeanTot, zMeanTot, xObsVal, yObsVal, zObsVal;
+        double sumX, sumY, meanXVal, meanYVal, sampXMean, sampYMean, xSSWG, ySSWG, xMeanTot, yMeanTot, xObsVal, yObsVal;
         vector<double> obs;
         for(size_t elements = 0; elements < x.size(); elements++){
             sumX += x[elements];
             sumY += y[elements];
-            sumZ += y[elements];
         }
         // Mean
         meanXVal = sumX / x.size();
+        cout<<"Mean X"<< meanXVal<<endl;
         meanYVal = sumY / y.size();
-        meanZVal = sumZ / z.size();
-        // Sum(Sample - Mean)
-        for(size_t elements = 0; elements < x.size(); elements++){
-            double diff = x[elements] - meanXVal;
-            sampXMean += diff;
-        }
-        for(size_t elements = 0; elements < y.size(); elements++){
-            double diff = y[elements] - meanYVal;
-            sampYMean += diff;
-        }
-          for(size_t elements = 0; elements < z.size(); elements++){
-            double diff = z[elements] - meanZVal;
-            sampZMean += diff;
-        }
+        cout<<"Mean Y"<< meanYVal<<endl;
+
         // SSW (Sum of Squares within groups) = Sum(Square(Sample - Mean))
         for(size_t elements = 0; elements < x.size(); elements++){
             double diff = x[elements] - meanXVal;
@@ -42,12 +30,8 @@ void anova(vector<double> x, vector<double> y, vector<double> z){
             double sqr = pow(diff, 2);
             ySSWG += sqr;
         }
-        for(size_t elements = 0; elements < z.size(); elements++){
-            double diff = z[elements] - meanZVal;
-            double sqr = pow(diff, 2);
-            zSSWG += sqr;
-        }
-        double sswg = xSSWG + ySSWG + zSSWG;
+
+        double sswg = xSSWG + ySSWG;
         cout<<"The Sum of Square within group: "<<sswg<<endl;
 
     // Each Groups: TSS (Total Sum of Squares):
@@ -55,10 +39,9 @@ void anova(vector<double> x, vector<double> y, vector<double> z){
         for(size_t elements = 0; elements < x.size(); elements++){
             xMeanTot += x[elements];
             yMeanTot += y[elements];
-            zMeanTot += z[elements];
         }
-        double sizeXYZ = x.size() + y.size() + z.size();
-        double meanTotObs = (xMeanTot + yMeanTot + zMeanTot) / sizeXYZ;
+        double sizeXY = x.size() + y.size();
+        double meanTotObs = (xMeanTot + yMeanTot) / sizeXY;
         cout<<"TSS Mean: "<<meanTotObs<<endl;
         // observation[index] - mean, Square (observation[index] - mean), and Sum(Square(Observation[index] - mean))
         for(size_t elements = 0; elements < x.size(); elements++){
@@ -71,36 +54,40 @@ void anova(vector<double> x, vector<double> y, vector<double> z){
             double sqr = pow(yObs, 2);
             yObsVal += sqr;
         }
-        for(size_t elements = 0; elements < z.size(); elements++){
-            double yObs = z[elements] - meanTotObs;
-            double sqr = pow(yObs, 2);
-            zObsVal += sqr;
-        }
-        double tss = xObsVal + yObsVal + zObsVal;
+        double tss = xObsVal + yObsVal;
         cout<<"The total sum of squares is: "<<tss<<endl;
 
     // Calculate Sum of Squares between groups
         // Sum of Squares between groups = Total sum of squares - Sum of squares within groups
         double xMeanSSBG = meanXVal - meanTotObs;
+        // cout<<"xMeanSSBG: "<<meanXVal<<" - "<<meanTotObs<<": "<<xMeanSSBG<<endl;
         double yMeanSSBG = meanYVal - meanTotObs;
-        double zMeanSSBG = meanZVal - meanTotObs;
-        double ssBG = xMeanSSBG + yMeanSSBG + zMeanSSBG;
+        // cout<<"yMeanSSBG: "<<meanYVal<<" - "<<meanTotObs<<": "<<yMeanSSBG<<endl;
+        double ssBG = (pow(xMeanSSBG,2) + pow(yMeanSSBG,2))*x.size();
         cout<<"The Sum of Squares between groups: "<<ssBG<<endl;
+        cout<<"The total sum of square: "<<tss<<" = "<<"SSBG: "<<ssBG<<" + "<<"ssWG: "<<sswg<<endl;
         // Total sum of squares = Sum of Squares between groups + Sum of squares within groups
 
     // Final Calculations
         // degress of freedom - 1 which is (total no.of groups) - 1
         // a = Sum of Squares between group / degrees of freedom 
+        double finalSSBG = ssBG / 1;
 
         // degrees of freedom = total observations - total no.of groups
         // b = Sum of squares within groups / degrees of freedom
-
-        // F = a / b
+        double finalSSWG = sswg / ((x.size()+y.size())-3);
+        double res = finalSSBG / finalSSWG;
+        cout<<"Before F-Statistics: "<<"F("<<finalSSBG<<","<<finalSSWG<<")"<<"is: "<<res<<endl;
+        // Refer F-Statistics
+        // Consider degrees of freedom from numerator from Horizontal perspective which is SSBG
+        // Consider degrees of freedom from numerator from Vertical perspective which is SSWG
+        // In this case the degrees of freedom in horizontal perspective is 1 (Groups - 1)
+        // In this case the degrees of freedom in horizontal perspective is 7 (Total observations - 3)
+        return res;
 }
 
 int main(){
     vector<double> x = {2,3,7,2,6};
     vector<double> y = {10,8,7,5,10};
-    vector<double> z = {10,13,14,13,15};
-    anova(x,y,z);
+    cout<<anova(x,y);
 }
