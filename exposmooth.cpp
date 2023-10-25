4794a0decaaa7fcab9cca0c1958b2bb50fba34f1
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-10-25 18:23:58 +0300

    Exponential Smoothening

--- exposmooth.cpp
+++ exposmooth.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 vector<double> exposmooth(vector<double> x, double alpha){
     // SES_t = α * Y_t + (1 - α) * SES_(t-1)
-    vector<double> result;
-    int fStep = 0;
-    int bStep = 1;
+    vector<double> expoSmooth={x[0]};
+    int fStep = 1;
+    int bStep = 0;
     for(size_t elements = 0; elements < x.size(); elements++){
-        double alphAct = alpha * x[fStep];
-        double smooth = (1-alpha) * x[bStep];
-        // cout<<"AplhaAct: "<<alphAct<<" + "<<smooth<<endl;
-        double out = alphAct + smooth;
-        result.push_back(out);
+        double alphAct = alpha * x[fStep]; // 0.2 * 10, 0.2 * 20, 0.2 * 30,....
+        double smoothF = (1-alpha) * expoSmooth[elements];
+        cout<<"AplhaAct: "<<alphAct<<" + "<<smoothF<<endl;
+        double outF = alphAct + smoothF;
+        expoSmooth.push_back(outF);
         fStep = fStep + 1;
         bStep = bStep - 1;
     }
-    return result;
+    return expoSmooth;
 }
 
 int main(){
